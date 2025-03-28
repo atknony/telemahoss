a6d9cbcb7aadb9c06ca0999f6334cf138b7f8865
ivorionn <yagiz.tuten@gmail.com>
2025-03-28 17:45:05 +0300

    last

---
+++ Scripts/game.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var enemy_scene : PackedScene
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func enemy_spawner() -> void:
+	pass

8f9694989e6b438e086b4bc593e408e925d31eda
ivorionn <yagiz.tuten@gmail.com>
2025-04-08 16:14:23 +0300

    1 ve 2 Tuşuyla turret upgrade

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	var direction = Vector2.RIGHT.rotated(rotation - PI / 2)
-	position += direction * 2
+	position += direction * 4
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if Right.is_colliding():
--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,15 +1,29 @@
 extends Node2D
 
+@export var level_2_turret : PackedScene
 @export var enemy_scene : PackedScene
+
+@onready var turret1 = $Turret1
+@onready var turret2 = $Turret2
 @onready var wave_started = true
 @onready var label = $Label
 @onready var money = 50
 
 func _process(delta: float) -> void:
 	label.text = str(money)
+	if Input.is_action_pressed("Upgrade1"):
+		upgrade_turret(turret1)
+	elif Input.is_action_pressed("Upgrade2"):
+		upgrade_turret(turret2)
 
 func enemy_spawn() -> void:
 	if wave_started:
 		var enemy = enemy_scene.instantiate()
 		add_child(enemy)
 		enemy.position = Vector2(0,358)
+
+func upgrade_turret(turret_scene):
+	var level2_turret = level_2_turret.instantiate()
+	level2_turret.position = turret_scene.position
+	remove_child(turret_scene)
+	add_child(level2_turret)

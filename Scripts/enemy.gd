822b621b17d1002035719e94ef5030721f1d2b92
ivorionn <yagiz.tuten@gmail.com>
2025-04-10 02:11:37 +0300

    Oynanabilir bir Oyun halinde
    W: Yeni Wave
    1: Soldaki tareti 1000 coinle upgradele
    2: Sağdaki tareti 1000 coinle upgradele

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,14 +3,14 @@ extends Area2D
 @onready var Right = $Right
 @onready var Left = $Left
 @onready var life = $Timer
-@onready var health = 20
+@onready var health = 3
 
 func _ready() -> void:
 	add_to_group("Enemies")
 
 func _process(delta: float) -> void:
 	var direction = Vector2.RIGHT.rotated(rotation - PI / 2)
-	position += direction * 4
+	position += direction * 2
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if Right.is_colliding():
@@ -25,6 +25,6 @@ func _on_timer_timeout() -> void:
 func _on_area_entered(area: Area2D) -> void:
 	if (area.is_in_group("Bullet")):
 		health -= 1
-		if health == 0:
+		if health <= 0:
 			get_parent().money += 10
 			queue_free()
--- Scripts/game.gd
+++ Scripts/game.gd
@@ -3,14 +3,17 @@ extends Node2D
 @export var level_2_turret : PackedScene
 @export var enemy_scene : PackedScene
 
+@onready var wave_level = 0
+@onready var Wave = $Wave
 @onready var turret1 = $Turret1
 @onready var turret2 = $Turret2
-@onready var wave_started = true
-@onready var label = $Label
-@onready var money = 3000
+@onready var wave_started = false
+@onready var label = $Balance
+@onready var money = 10000
 
 func _process(delta: float) -> void:
-	label.text = str(money)
+	label.text = "Balance: " + str(money)
+	Wave.text = "Wave: " + str(wave_level)
 	if Input.is_action_just_pressed("Upgrade1"):
 		if money >= 1000:
 			money -= 1000
@@ -19,15 +22,20 @@ func _process(delta: float) -> void:
 		if money >= 1000:
 			money -= 1000
 			upgrade_turret(turret2)
+	elif Input.is_action_just_pressed("NextWave") && wave_started == false:
+		wave_started = true
+		wave_level += 1
+		for i in range(1,wave_level * 3):
+			enemy_spawn()
+			await get_tree().create_timer(0.4).timeout
+			if i == (wave_level * 3 - 1):
+				wave_started = false
 
 func enemy_spawn() -> void:
-	if wave_started:
-		var enemy = enemy_scene.instantiate()
-		add_child(enemy)
-		enemy.position = Vector2(0,358)
+	var enemy = enemy_scene.instantiate()
+	add_child(enemy)
+	enemy.health = wave_level * 1.1
+	enemy.position = Vector2(0,358)
 
 func upgrade_turret(turret_scene):
-	var level2_turret = level_2_turret.instantiate()
-	level2_turret.position = turret_scene.position
-	remove_child(turret_scene)
-	add_child(level2_turret)
+	turret_scene.fire_rate /= 1.1

d1183e7053b915a7c0deb2804fc835d2a28ed217
ivorionn <yagiz.tuten@gmail.com>
2025-04-12 01:27:11 +0300

    Turret Rangeden çıkınca artık ateş etmiyor

--- Scripts/turret.gd
+++ Scripts/turret.gd
@@ -11,10 +11,10 @@ func _process(delta):
 	fire_timer -= delta
 	var target = get_closest_enemy()
 
-	if fire_timer <= 0 and target:
+	if fire_timer <= 0 and target in enemies_in_range:
 		shoot(target)
 		fire_timer = fire_rate
-	if target:
+	if target and target in enemies_in_range:
 		rotation = (target.global_position - global_position).angle() + PI / 2
 
 func get_closest_enemy() -> Area2D:
@@ -40,8 +40,9 @@ func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, 
 		enemies_in_range.append(area)
 
 func _on_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	if area.is_in_group("Enemies"):
-		enemies_in_range.erase(area)
+	if area:
+		if area.is_in_group("Enemies"):
+			enemies_in_range.erase(area)
 
 func _on_panel_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
